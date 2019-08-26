3c5d42d23fb8ba726a4711d756e28ceb77fe4723
Michael Hazzard <miramardesign@gmail.com>
2019-08-26 19:03:12 -0300

    Updates

---
+++ precache-manifest.beae15c82cc7177cb6ce695837d5266c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dd789b3d441552a24efa75f80de3ca3",
+    "url": "/react-pwa-crypto/index.html"
+  },
+  {
+    "revision": "b3b62967eb8ebcffa6b6",
+    "url": "/react-pwa-crypto/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4e56a7356c12372a74fb",
+    "url": "/react-pwa-crypto/static/js/2.c7274c25.chunk.js"
+  },
+  {
+    "revision": "b3b62967eb8ebcffa6b6",
+    "url": "/react-pwa-crypto/static/js/main.5626213e.chunk.js"
+  },
+  {
+    "revision": "13db564cab307283df1a",
+    "url": "/react-pwa-crypto/static/js/runtime~main.c34137df.js"
+  }
+]);
